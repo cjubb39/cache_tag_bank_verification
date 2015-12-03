27bbb39d2b84ba9324df6847045e7672df0bf11a
Chae Jubb <cjubb39@gmail.com>
2015-12-03 14:04:47 -0500

    latency constraint without flushing holds

--- directives.do
+++ directives.do
@@ -1,6 +1,11 @@
 #define clock
-netlist clock clk -period 100
+netlist clock clk -period 10
 
 # constrain reset
-#formal netlist constraint rst 1'b1
+formal netlist constraint rst 1'b1
 formal netlist constraint flush_in_valid 1'b0
+
+# one synchronous request in flight at a time
+formal netlist constraint way_out_ready 1'b1
+formal netlist constraint state_out_ready 1'b1
+formal netlist constraint inv_ack_cnt_out_ready 1'b1
--- src/sva_l2_cache_tag.sv
+++ src/sva_l2_cache_tag.sv
@@ -58,6 +58,78 @@ input     mem_inv_ack_cnt_CE5, mem_inv_ack_cnt_CE6, mem_inv_ack_cnt_CE7, mem_inv
 default clocking c0 @(posedge clk); endclocking
 
 /*
+ * ASSUMPTIONS
+ */
+
+/* TODO currently overconstrained */
+tag_in_valid_fc_0:
+  assume property (disable iff(!rst) $rose(tag_in_valid) |-> tag_in_ready);
+set_in_valid_fc_0:
+  assume property (disable iff(!rst) $rose(set_in_valid) |-> set_in_ready);
+state_in_valid_fc_0:
+  assume property (disable iff(!rst) $rose(state_in_valid) |-> state_in_ready);
+inv_ack_cnt_in_valid_fc_0:
+  assume property (disable iff(!rst) $rose(inv_ack_cnt_in_valid) |-> inv_ack_cnt_in_ready);
+
+/* flex channel semantics */
+tag_in_valid_fc_1:
+  assume property (disable iff(!rst) tag_in_valid && tag_in_ready |=> !tag_in_valid);
+set_in_valid_fc_1:
+  assume property (disable iff(!rst) set_in_valid && set_in_ready |=> !set_in_valid);
+state_in_valid_fc_1:
+  assume property (disable iff(!rst) state_in_valid && state_in_ready |=> !state_in_valid);
+inv_ack_cnt_in_valid_fc_1:
+  assume property (disable iff(!rst) inv_ack_cnt_in_valid && inv_ack_cnt_in_ready |=> !inv_ack_cnt_in_valid);
+
+/* come as a group */
+nom_input_0:
+  assume property (disable iff(!rst)
+    (tag_in_valid || set_in_valid || state_in_valid || inv_ack_cnt_in_valid) |->
+      (tag_in_valid && set_in_valid));
+
+/* one request in flight at a time */
+tag_in_valid_fc_2:
+  assume property (disable iff(!rst) $fell(tag_in_valid) |-> !tag_in_valid[*] ##1 way_out_valid);
+set_in_valid_fc_2:
+  assume property (disable iff(!rst) $fell(set_in_valid) |-> !set_in_valid[*] ##1 way_out_valid);
+state_in_valid_fc_2:
+  assume property (disable iff(!rst) $fell(state_in_valid) |-> !state_in_valid[*] ##1 way_out_valid);
+inv_ack_cnt_in_valid_fc_2:
+  assume property (disable iff(!rst) $fell(inv_ack_cnt_in_valid) |-> !inv_ack_cnt_in_valid[*] ##1 way_out_valid);
+
+/* state and inv_ack are optional so we condition also on tag */
+state_in_valid_fc_3:
+  assume property (disable iff(!rst) $fell(tag_in_valid) |-> !state_in_valid[*] ##1 way_out_valid);
+inv_ack_cnt_in_valid_fc_3:
+  assume property (disable iff(!rst) $fell(tag_in_valid) |-> !inv_ack_cnt_in_valid[*] ##1 way_out_valid);
+
+/* one synchronous request in flight at a time */
+way_out_valid_fc_0:
+  assume property (disable iff(!rst) $rose(way_out_valid) |-> way_out_ready);
+state_out_valid_fc_0:
+  assume property (disable iff(!rst) $rose(state_out_valid) |-> state_out_ready);
+inv_ack_cnt_out_valid_fc_0:
+  assume property (disable iff(!rst) $rose(inv_ack_cnt_out_valid) |-> inv_ack_cnt_out_ready);
+
+way_out_flush_valid_fc_0:
+  assume property (disable iff(!rst) $rose(way_out_flush_valid) |-> way_out_flush_ready);
+set_out_flush_valid_fc_0:
+  assume property (disable iff(!rst) $rose(set_out_flush_valid) |-> set_out_flush_ready);
+tag_out_flush_valid_fc_0:
+  assume property (disable iff(!rst) $rose(tag_out_flush_valid) |-> tag_out_flush_ready);
+state_out_flush_valid_fc_0:
+  assume property (disable iff(!rst) $rose(state_out_flush_valid) |-> state_out_flush_ready);
+inv_ack_cnt_out_flush_valid_fc_0:
+  assume property (disable iff(!rst) $rose(inv_ack_cnt_out_flush_valid) |-> inv_ack_cnt_out_flush_ready);
+
+set_out_evict_valid_fc_0:
+  assume property (disable iff(!rst) $rose(set_out_evict_valid) |-> set_out_evict_ready);
+tag_out_evict_valid_fc_0:
+  assume property (disable iff(!rst) $rose(tag_out_evict_valid) |-> tag_out_evict_ready);
+state_out_evict_valid_fc_0:
+  assume property (disable iff(!rst) $rose(state_out_evict_valid) |-> state_out_evict_ready);
+
+/*
  * BASIC COVERS
  */
 
@@ -75,7 +147,7 @@ inv_ack_cnt_in_valid_cover_0: cover property (!inv_ack_cnt_in_valid);
 inv_ack_cnt_in_valid_cover_1: cover property (inv_ack_cnt_in_valid);
 
 flush_in_valid_cover_0: cover property (!flush_in_valid);
-//flush_in_valid_cover_1: cover property (flush_in_valid);
+flush_in_valid_cover_1: cover property (flush_in_valid);
 flush_complete_ready_cover_0: cover property (!flush_complete_ready);
 flush_complete_ready_cover_1: cover property (flush_complete_ready);
 
@@ -114,12 +186,10 @@ state_in_ready_cover_1: cover property (state_in_ready);
 inv_ack_cnt_in_ready_cover_0: cover property (!inv_ack_cnt_in_ready);
 inv_ack_cnt_in_ready_cover_1: cover property (inv_ack_cnt_in_ready);
 
-/*
 flush_in_ready_cover_0: cover property (!flush_in_ready);
 flush_in_ready_cover_1: cover property (flush_in_ready);
 flush_complete_valid_cover_0: cover property (!flush_complete_valid);
 flush_complete_valid_cover_1: cover property (flush_complete_valid);
-*/
 
 way_out_valid_cover_0: cover property (!way_out_valid);
 way_out_valid_cover_1: cover property (way_out_valid);
@@ -135,7 +205,6 @@ set_out_evict_valid_cover_1: cover property (set_out_evict_valid);
 state_out_evict_valid_cover_0: cover property (!state_out_evict_valid);
 state_out_evict_valid_cover_1: cover property (state_out_evict_valid);
 
-/*
 way_out_flush_valid_cover_0: cover property (!way_out_flush_valid);
 way_out_flush_valid_cover_1: cover property (way_out_flush_valid);
 tag_out_flush_valid_cover_0: cover property (!tag_out_flush_valid);
@@ -146,11 +215,9 @@ state_out_flush_valid_cover_0: cover property (!state_out_flush_valid);
 state_out_flush_valid_cover_1: cover property (state_out_flush_valid);
 inv_ack_cnt_out_flush_valid_cover_0: cover property (!inv_ack_cnt_out_flush_valid);
 inv_ack_cnt_out_flush_valid_cover_1: cover property (inv_ack_cnt_out_flush_valid);
-*/
 
 
 /* memory covers */
-/*
 mem_tag_CE0_cover_0: cover property (!mem_tag_CE0);
 mem_tag_CE0_cover_1: cover property (mem_tag_CE0);
 mem_tag_WE0_cover_0: cover property (!mem_tag_WE0);
@@ -213,6 +280,13 @@ mem_inv_ack_cnt_CE7_cover_0: cover property (!mem_inv_ack_cnt_CE7);
 mem_inv_ack_cnt_CE7_cover_1: cover property (mem_inv_ack_cnt_CE7);
 mem_inv_ack_cnt_CE8_cover_0: cover property (!mem_inv_ack_cnt_CE8);
 mem_inv_ack_cnt_CE8_cover_1: cover property (mem_inv_ack_cnt_CE8);
-*/
+
+/*
+ * LATENCY CONSTRAINTS
+ */
+rsp_latency_0:
+  assert property (disable iff(!rst)
+    ($rose(tag_in_valid) && $rose(set_in_valid) && tag_in_ready && set_in_ready && !flush_in_valid) |->
+      ##[0:50] ($rose(way_out_valid)));
 
 endmodule
